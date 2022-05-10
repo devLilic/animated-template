0966f051318885bfc62d0de62368063f55ea9273
Lilic <zubcolilian@mail.ru>
2022-05-10 13:43:58 +0300

    final project

---
+++ js/script.js
@@ -0,0 +1,5 @@
+AOS.init({
+    delay: 300, // values from 0 to 3000, with step 50ms
+    duration: 500, // values from 0 to 3000, with step 50ms
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
